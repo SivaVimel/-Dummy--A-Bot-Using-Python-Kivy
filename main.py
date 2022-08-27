0c40b4909fd0f74c8385d90de7d4f6d8afd05082
SivaVimel <87802556+SivaVimel@users.noreply.github.com>
2022-08-27 12:03:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ kivy.require('1.7.1')
 from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from sys import path
 from instaloader.exceptions import ProfileNotExistsException
-import pyttsx3
 import speech_recognition as sr
 import datetime
-import pyaudio
 import wikipedia
 import webbrowser
 import random
@@ -29,18 +28,18 @@ class SayThis(BoxLayout):
     
     def say_something(self, text):
         try:
-            text = text.lower() #To convert the input text into lowercase
-            
-            #keywords reply - to take random output
-            fav = ["yes this has the kick", "for me too", "i know"]
-            how = ["i am genius","its magic","well you know","nice joke", "very well thank you"]
-            appreciation = ["thank you","I did the best","Yes, its my effort","i appreciate you too"]
-            we_did_it = ["yes, we did it","Not without me","Yes"]
-            hru = ["I am good, how about you", "i am good", "fine" ,"fine and you" ,"i am fine and how is it for you"]
-            gd = ["You should always be happy, thats your favorite thing and i am here for you always on your command", "its good to hear", "great", "its good to hear you are happy"]
-            bd = ["you are not alone", "if you are alone then i am also alone"]
-            food = ["You should probably eat" , "Your health is important", "do you want me to open online food shops?" , "Do you want me to call any food service"]
-            mfav = ["thank you", "I appreciate that", "I am also my favourite"]
+            #keywords reply - to give random output
+            fav = ["yes boss this has the kick", "for me too boss", "i know boss"]
+            how = ["you programmed me boss","its magic","well you know","nice joke boss", "very well thank you"]
+            appreciation = ["thank you boss","not without you boss","no you did the best boss","its your effort boss","i appreciate you too boss"]
+            we_did_it = ["yes boss, we did it","not without you boss","yes boss"]
+            hru = ["i am good, how about you boss", "i am good boss", "fine boss" ,"fine boss and you" ,"i am fine boss and how is it for you"]
+            gd = ["You should always be happy boss, thats your favorite thing and i am here for you always on your command", "its good to hear boss", "great boss", "its good to hear you are happy boss"]
+            bd = ["you are not alone boss", "if you are alone then i am also alone boss"]
+            food = ["boss, you should probably eat" , "Your health is important boss", "do you want me to open online food shops?" , "boss do you want me to call any food service"]
+            mfav = ["thank you boss", "I appreciate that boss", "if you feel it  your my favorite too boss"]
+            name = ["My name is JARVIS","JARVIS","I am JARVIS","I am Just A Really Very Intelligent System, JARVIS an AI"]
+            areu = ["I am Just A Really Very Intelligent System","I am JARVIS, not an alien nor a human, i am an AI","I am JARVIS made with inspiration of Tony Stark"]
             bst = ["Yes, I am","Yes, I am the best","Yes I know","You have brain","People always say that"]
             ty = ["Gracias","I am the best","Thank You","Well Welcome",":D"]
             okay = ["Yes, do you want anything else?","Yes","Do you have anything to ask?",]
@@ -50,12 +49,12 @@ class SayThis(BoxLayout):
             like = ["I like Space and Ocean","I am into Space and Ocean","I am currently in love with Space and Ocean"]
             jellyfish = ["It's understandable that you might not want to encounter \na shark during your swim, but the pesky jellyfish—of which \nthere are millions in the ocean—may actually be a greater threat to your life. \nThanks to their venom and relative lack of detectability underwater, \njellyfish have a body count that's actually five times higher than that of sharks. \nAgain, we've never encountered a single creature this terrifying out in the cosmos."]
             scary = ["Nothing scares me rather than space and ocean and also EXIT BUTTON","Human's should be terrified of Space and Ocean","Anything can come out of space and ocean","Earth can be swallowed by space or ocean, jk nothing will happen ;D"]
-            wru =["I am Dummy","My name is Dummy","I am Dummy, you can call me Dummy"]
-            yourname = ["I am Dummy, a dummy bot created to ba your friend in talk","I am Dummy, not a alien or ghost but just a bot","I am Dummy for sure"]
             wdl = ["I am living in your device but i am not a parasite for sure","I have rented to part of your device","I am living in a device, that's currently in planet earth"]
             youruse = ["I thought I am Dummy, not you","I am useful to be not useful","I can respond to some commands.\n Type 'help' for commands list"]
             
             elsehere = ["What do you mean 🎶 ~JB","Did you forget my name is Dummy, not Google","well i dont know what the heck you are saying","What are you even saying","I am a child","Oh No","Out Of Syllabus","Just Go and Live","Take A Hike Mate, \n Live A Life","One Day I May Reply For This \nBut Not Today"]
+            
+            
             #music commands -- 
             if 'play motivational' in text:
                 tts.speak("boss i am playing some motivational songs for you")
@@ -82,43 +81,33 @@ class SayThis(BoxLayout):
                 tts.speak("boss here you go the naughty tracks")
                 webbrowser.open_new_tab(
                     'https://www.youtube.com/watch?v=OORoOGY8D2M&list=PLWc9sw89ZYSmh8k_QqHXDgPuQILo7QBFN')
-            elif 'my beat' in text or 'play music' in text:
+            elif 'my beat' in text:
                 tts.speak("Yes boss, playing your listed songs")
                 webbrowser.open_new_tab(
                     'https://www.youtube.com/watch?v=kudi8OtMu9s&list=PLWc9sw89ZYSkJkqF8M3aCKuxPS8jrUaiP')
                    
                
             # greetings
-            elif "hey" in text or "hello" in text or "i am back" in text or "hola" in text:
-                hour = int(datetime.datetime.now().hour)
-                if hour >= 0 and hour <= 12:
-                    tts.speak("Good Morning")
-                elif hour >= 12 and hour < 18:
-                    tts.speak("Good Afternoon")
-                else:
-                    tts.speak("Good Evening")   
             elif "bye" in text or "go to sleep" in text:
                 hour = int(datetime.datetime.now().hour)
                 if hour >= 6 and hour <= 15:
-                    tts.speak("Good Bye, enjoy your day")
+                    tts.speak("Good Bye boss, enjoy your day")
                     exit()
                 elif hour >= 15 and hour <= 19:
-                    tts.speak("Good Bye, enjoy your evening")
+                    tts.speak("Good Bye boss, enjoy your evening")
                     exit()
                 elif hour >= 19 and hour <= 23:
-                    tts.speak("Good Bye, enjoy your night")
+                    tts.speak("Good Bye boss, enjoy your night")
                     exit()
                 else:
                     tts.speak("Good Night boss")
-                    exit()      
+                    exit()        
             elif 'the best' in text:
-                tts.speak(random.choice(bst))
-            elif 'ok' in text or 'okay' in text or "kk" in text:
-                tts.speak(random.choice(okay))
-            elif 'no' in text or 'nope' in text or "na" in text:
-                tts.speak(random.choice(nope))
+                tts.speak("you are the best boss")
+            elif 'ok dream' in text:
+                tts.speak("Yes boss, do you want me to do something for you boss")
             elif 'thank you' in text or 'thankyou' in text:
-                tts.speak(random.choice(ty))
+                tts.speak("Your welcome boss")
             elif 'good work' in text or 'nice job' in text:
                 tts.speak("Thank You boss") 
             
@@ -138,6 +127,13 @@ class SayThis(BoxLayout):
                 webbrowser.open_new_tab(url)
                 tts.speak('Here is What I found for' + search) 
 
+                
+            elif 'wikipedia' in text:
+                text = text.replace("wikipedia", "")
+                search = text
+                url = 'https://en.wikipedia.org/wiki/' + search
+                webbrowser.open_new_tab(url)
+                tts.speak('Here is What I found for' + search)
                 
             elif 'what is' in text or 'tell me about' in text:
                 text = text.replace("what is", "")
@@ -172,8 +168,8 @@ class SayThis(BoxLayout):
                 url = 'https://google.nl/maps/place/' + location + '/&amp;'
                 webbrowser.open_new_tab(url)
                 tts.speak('Here is the location ' + location)
-                
-            #Download YouTube Video
+            
+            #Download YouTbe Video
             elif 'a youtube video' in text:
                 text = text.replace("a youtube video", "")
                 url = text
@@ -187,7 +183,6 @@ class SayThis(BoxLayout):
                 tts.print(best.resolution, best.extension)
                 best.download()
                 tts.speak("Its done boss, i have downloaded the video your asked")
-                
             #Download YouTube Audio
             elif 'a youtube audio' in text or 'a youtube song' in text:
                 text = text.replace("a youtube audio", "")
@@ -199,7 +194,7 @@ class SayThis(BoxLayout):
                 bestaudio.download()
                 tts.speak("boss i have downloaded the audio your asked")
             
-            #Get Instagram Profile Details via instaginasta.com
+            #Get Instagram Profile Details via instafinsta,com
             elif 'insta stuff' in text:
                 webbrowser.open('https://instafinsta.com/')
                 tts.speak("boss here is the result")    
@@ -230,19 +225,45 @@ class SayThis(BoxLayout):
                 tts.speak("do you need your energy pill boss?")
                 tts.speak("just tell me which music pill you prefer")
             elif 'drama queen' in text:
-                tts.speak("No, your girlfriend is the drama queen")
+                tts.speak("No boss, your girlfriend is the drama queen")
             elif 'i am hungry' in text or 'i am starving' in text or 'did not eat food' in text or 'have not ate food' in text or 'did not ate food' in text:
                 tts.speak(random.choice(food))
             elif 'you are my favourite' in text :
                 tts.speak(random.choice(mfav))
+            elif 'your name' in text:
+                tts.speak(random.choice(name))
+            elif 'are you' in text or 'are you a' in text:
+                tts.speak(random.choice(areu))    
+            elif 'created you' in text:
+                tts.speak("Siva Vimel Rajhen")
+            elif 'the best' in text:
+                tts.speak(random.choice(bst))
+            elif 'ok' in text or 'okay' in text or "kk" in text:
+                tts.speak(random.choice(okay))
+            elif 'no' in text or 'nope' in text or "na" in text:
+                tts.speak(random.choice(nope))
+            elif 'thank you' in text or 'thankyou' in text:
+                tts.speak(random.choice(ty))
+            elif 'good work' in text or 'nice job' in text:
+                tts.speak("Thank You boss")
+            
+            # "remember that (TEXT)" - "TEXT" would be saved in data.txt
+            elif 'remember that' in text:
+                text = text.replace("remember that", "")
+                rememberMessage = text
+                tts.speak("you said me to remember"+rememberMessage)
+                remember = open('data.txt', 'a')
+                remember.write(rememberMessage)
+                remember.close()
+            # "remember anything" - Gets "TEXT" from data.txt
+            elif 'remember anything' in text:
+                remember = open('data.txt', 'r')
+                tts.speak("you said me to remember that" + remember.read())
+            
             elif 'scary' in text or 'terrifying' in text:
                 tts.speak(random.choice(scary))
             elif 'you like' in text :
-                tts.speak(random.choice(like))
-            elif 'who are you' in text or 'what are you' in text:
-                tts.speak(random.choice(wru))
-            elif 'your name' in text or 'are you alien' in text or 'are you ai' in text or 'are you a bot' in text or 'are you bot' in text or 'dummy' in text or 'are you' in text or 'are you a' in text:
-                tts.speak(random.choice(yourname))
+                tts.speak(random.choice(like))    
             elif 'where are you' in text or 'your location' in text or 'where do you live' in text or 'which planet are you from' in text:
                 tts.speak(random.choice(wdl))
             elif 'your use' in text:
@@ -261,9 +282,9 @@ class SayThis(BoxLayout):
             else:
                 tts.speak(random.choice(elsehere))
                 tts.speak("Well I can do google search for you")
-                tts.speak("Type Search Title")
-
-        #Error--
+                tts.speak("Type Search Title")    
+            
+        #ERRORS--         
         except NotImplementedError:
             popup = Popup(title='TTS Not Implemented',
                           content=Label(text='Sorry. TTS is not available.'),
@@ -271,10 +292,17 @@ class SayThis(BoxLayout):
                           size=(300, 300))
             popup.open()
     
-    #Clear button     
+        
     def clear(self):
-        #removes text in InputBox with
-        self.saywhat_text.text = "" 
+        hour = int(datetime.datetime.now().hour)
+        if hour >= 0 and hour <= 12:
+            tts.speak("Good Morning boss")
+        elif hour >= 12 and hour < 18:
+            tts.speak("Good Afternoon boss")
+        else:
+            tts.speak("Good Evening boss")
+        tts.speak("How can i help you")
+        self.saywhat_text.text = ""
         self.saywhat_text.focus = True
 
 
